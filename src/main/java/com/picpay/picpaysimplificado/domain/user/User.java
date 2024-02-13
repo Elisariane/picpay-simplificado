dd31449eef26ffd6ff80ed490772022ae5bc925d
Elisariane <elisarianeBS@gmail.com>
2024-02-13 15:03:44 -0300

    feat: create User entity

---
+++ src/main/java/com/picpay/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,34 @@
+package com.picpay.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity(name = "users")
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
